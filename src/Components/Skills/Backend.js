d1f22eed4a6b2f806ec020684a08d698329bf5e9
Aathif-amed <92617395+Aathif-amed@users.noreply.github.com>
2023-01-11 22:59:33 +0530

    icons updated

--- src/Components/Skills/Backend.js
+++ src/Components/Skills/Backend.js
@@ -7,14 +7,14 @@ function Backend() {
     <div className="skills_box">
       <div className="skills_group">
         <div className="skills_data">
-          <box-icon name="badge-check"className="skills_icon"></box-icon>
+          <box-icon name="nodejs" type="logo"className="skills_icon"></box-icon>
           <div>
             <h3 className="skills_name">NodeJS</h3>
             <span className="skills_level">Intermediate</span>
           </div>
         </div>
         <div className="skills_data">
-          <box-icon name="badge-check"className="skills_icon"></box-icon>
+          <box-icon name="data" type="solid"className="skills_icon"></box-icon>
           <div>
             <h3 className="skills_name">SQL</h3>
             <span className="skills_level">Basic</span>
@@ -23,14 +23,14 @@ function Backend() {
         </div>
       <div className="skills_group">
         <div className="skills_data">
-          <box-icon name="badge-check"className="skills_icon"></box-icon>
+          <box-icon name="mongodb" type="logo"className="skills_icon"></box-icon>
           <div>
             <h3 className="skills_name">MongoDB</h3>
             <span className="skills_level">Intermediate</span>
           </div>
         </div>
         <div className="skills_data">
-          <box-icon name="badge-check"className="skills_icon"></box-icon>
+          <box-icon name="aws" type="logo"className="skills_icon"></box-icon>
           <div>
             <h3 className="skills_name">AWS EC2 & S3</h3>
             <span className="skills_level">Basic</span>
--- src/Components/Skills/Frontend.js
+++ src/Components/Skills/Frontend.js
@@ -8,21 +8,21 @@ function Frontend() {
       <div className="skills_box">
         <div className="skills_group">
           <div className="skills_data">
-            <box-icon name="badge-check" className="skills_icon"></box-icon>
+            <box-icon name="file-html" type="solid" className="skills_icon"></box-icon>
             <div>
               <h3 className="skills_name">HTML</h3>
               <span className="skills_level">Intermediate</span>
             </div>
           </div>
           <div className="skills_data">
-            <box-icon name="badge-check"className="skills_icon"></box-icon>
+            <box-icon name="file-css" type="solid" className="skills_icon"></box-icon>
             <div>
               <h3 className="skills_name">CSS</h3>
               <span className="skills_level">Intermediate</span>
             </div>
           </div>
           <div className="skills_data">
-            <box-icon name="badge-check"className="skills_icon"></box-icon>
+            <box-icon name='javascript' type='logo'className="skills_icon"></box-icon>
             <div>
               <h3 className="skills_name">JavaScript</h3>
               <span className="skills_level">Intermediate</span>
@@ -32,21 +32,21 @@ function Frontend() {
         <div className="skills_group">
 
           <div className="skills_data">
-            <box-icon name="badge-check"className="skills_icon"></box-icon>
+            <box-icon  name='bootstrap' type='logo' className="skills_icon"></box-icon>
             <div>
               <h3 className="skills_name">Bootstrap</h3>
               <span className="skills_level">Advanced</span>
             </div>
           </div>
           <div className="skills_data">
-            <box-icon name="badge-check"className="skills_icon"></box-icon>
+            <box-icon name='git' type='logo'className="skills_icon"></box-icon>
             <div>
               <h3 className="skills_name">Git</h3>
               <span className="skills_level">Intermediate</span>
             </div>
           </div>
           <div className="skills_data">
-            <box-icon name="badge-check"className="skills_icon"></box-icon>
+            <box-icon name='react' type='logo'className="skills_icon"></box-icon>
             <div>
               <h3 className="skills_name">React</h3>
               <span className="skills_level">Intermediate</span>
